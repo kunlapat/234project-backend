3c4639f66f51e1b61e95553e489b45b5807c767d
Unknown <risky4teen@gmail.com>
2018-05-07 16:49:02 +0700

    Test available Products
    -Add testAvailableProduct() in ProductsSercvicelmpTest class.
    -Passed

--- src/test/java/camt/se234/project/service/ProductServiceImplTests.java
+++ src/test/java/camt/se234/project/service/ProductServiceImplTests.java
@@ -4,6 +4,8 @@ import camt.se234.project.dao.ProductDao;
 import camt.se234.project.entity.Product;
 import org.junit.Before;
 import org.junit.Test;
+
+import static org.hamcrest.Matchers.hasItems;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.Mockito.mock;
@@ -41,6 +43,24 @@ public class ProductServiceImplTests {
         when(productDao.getProducts()).thenReturn(mockProducts);
 
         assertThat(productService.getAllProducts(),is(mockProducts));
+
+    }
+
+
+    @Test
+    public void testAvailableProduct(){
+
+        List<Product> mockProducts = new ArrayList<>();
+        mockProducts.add(new Product(null,"A01","Water","drink","",50.00));
+        mockProducts.add(new Product(null,"B01","Wheat","drink","",1000.00));
+        mockProducts.add(new Product(null,"C01","Ice","drink","",80.00));
+        mockProducts.add(new Product(null,"D01","Sang Som","drink","",-1));
+        mockProducts.add(new Product(null,"E01","Coca Cola","drink","",-1));
+        // give mockProducts index 3-4 have price = -1
+        when(productDao.getProducts()).thenReturn(mockProducts);
+
+        // return mockProducts which is price  > 0
+        assertThat(productService.getAvailableProducts(),hasItems(mockProducts.get(0),mockProducts.get(1)));
 
     }
 }
